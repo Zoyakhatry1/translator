fc9a065c97ee82171aef9e5e29b1a564ff11edb5
RajAgrahari <rajagri1682003@gmail.com>
2023-11-07 00:59:56 +0530

    added download btn

--- js/texttoword.js
+++ js/texttoword.js
@@ -1,41 +1,41 @@
 function ExportToDoc(element, filename = ''){
-        var header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
-     
-        var footer = "</body></html>";
-        var text = document.getElementById(element).value;
-        var html = header+`<p>${text}</p>`+footer;
-     
-        var blob = new Blob(['\ufeff', html], {
-            type: 'application/msword'
-        });
+    var header = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML to Word Document with JavaScript</title></head><body>";
+ 
+    var footer = "</body></html>";
+    var text = document.getElementById(element).value;
+    var html = header+`<p>${text}</p>`+footer;
+ 
+    var blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
+    
+    // Specify link url
+    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+    // Specify file name
+    filename = filename?filename+'.docx':'document.docx';
+    
+    // Create download link element
+    var downloadLink = document.createElement("a");
+ 
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = url;
         
-        // Specify link url
-        var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+        // Setting the file name
+        downloadLink.download = filename;
         
-        // Specify file name
-        filename = filename?filename+'.docx':'document.docx';
-        
-        // Create download link element
-        var downloadLink = document.createElement("a");
-     
-        document.body.appendChild(downloadLink);
-        
-        if(navigator.msSaveOrOpenBlob ){
-            navigator.msSaveOrOpenBlob(blob, filename);
-        }else{
-            // Create a link to the file
-            downloadLink.href = url;
-            
-            // Setting the file name
-            downloadLink.download = filename;
-            
-            //triggering the function
-            downloadLink.click();
-        }
-        
-        document.body.removeChild(downloadLink);
+        //triggering the function
+        downloadLink.click();
+    }
+    
+    document.body.removeChild(downloadLink);
 }
 
-function foo(ele){
-    console.log( document.getElementById(ele).value.split("\n"))
-}
+// function foo(ele){
+//     console.log( document.getElementById(ele).value.split("\n"))
+// }
